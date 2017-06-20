605bc9bdf4ede51877cc05e0d5d03127a0d4da51
Daeseon Kim <stict77@gmail.com>
2017-06-20 04:54:05 +0000

    Created our first APIView.

---
+++ profiles_api/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+
+# from profiles_api import models
+from . import models
+
+# Register your models here.
+
+admin.site.register(models.UserProfile)
+
---
+++ profiles_api/urls.py
@@ -0,0 +1,8 @@
+from django.conf.urls import url
+
+from profiles_api import views
+
+urlpatterns = [
+    url(r'^hello-view/', views.HelloApiView.as_view()),
+
+]
