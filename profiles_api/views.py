ea468fab9ca7f60d9751914dcccb8d82587b7b6e
Daeseon Kim <stict77@gmail.com>
2017-06-21 07:07:50 +0000

    Added POST, PUT, PATCH and DELETE functions to our APIView

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -2,12 +2,18 @@ from django.shortcuts import render
 
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
 
+from profiles_api import serializers
 
 # Create your views here.
 
+
 class HelloApiView(APIView):
     """Test API View."""
+
+    # 이 APIView 의 시리얼라이저는 요놈이다라고 Django or DRF 에게 알려줌
+    serializer_class = serializers.HelloSerializer
 
     def get(self, request, format=None):
         """Returns a list of APIView features."""
@@ -19,5 +25,32 @@ class HelloApiView(APIView):
             'Is mapped manually to URLs'
         ]
 
-        # DRF는 Response를 JSON Format 변경해 줌.
+        # DRF는 Response를 JSON Format 으로 이쁘게 보여줌 (변경은 아님).
         return Response({'message': 'Hello!', 'an_apiview': an_apiview})
+
+    def post(self, request):
+        """Create a hello message with our name."""
+
+        serializer = serializers.HelloSerializer(data=request.data)
+
+        if serializer.is_valid():
+            name = serializer.data.get('name')
+            message = 'Hello {0}'.format(name)
+            return Response({'message': message})
+        else:
+            return Response(
+                serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def put(self, request, pk=None):
+        """Handles updating an object."""
+
+        return Response({'method': 'put'})
+
+    def patch(self, request, pk=None):
+        """Patch request, only updates fields provided in the request."""
+        return Response({'method': 'patch'})
+
+    def delete(self, request, pk=None):
+        """Deletes and object."""
+
+        return Response({'method': 'delete'})
