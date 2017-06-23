e88d10c6007fd80de3b8806888112f2311e5ed93
Daeseon Kim <stict77@gmail.com>
2017-06-23 08:30:56 +0000

    Creating User Profile using ModelSerializer.

--- profiles_api/serializers.py
+++ profiles_api/serializers.py
@@ -1,7 +1,18 @@
 from rest_framework import serializers
 
+from profiles_api import models
+
 class HelloSerializer(serializers.Serializer):
     """Serializes a name field for testing our APIView."""
 
     name = serializers.CharField(max_length=10)
     # nid = serializers.CharField(max_length=10)
+
+
+class UserProfileSerializer(serializers.ModelSerializer):
+    """A serializer for our user profile objects."""
+
+    class Meta:
+        model = models.UserProfile
+        fields = ('id', 'email', 'name', 'password')
+        extra_kwargs = {'password': {'write_only': True}}
